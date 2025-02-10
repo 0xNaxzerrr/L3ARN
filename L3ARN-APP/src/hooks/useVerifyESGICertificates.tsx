db72db953b9eb00c2f2af8409f9ff7ffaea50eaf
0xNaxzerrr <naxdev75@gmail.com>
2025-02-10 15:19:48 +0100

    feat: Enhance useVerifyESGICertificates hook to support performance NFT verification

--- L3ARN-APP/src/hooks/useVerifyESGICertificates.tsx
+++ L3ARN-APP/src/hooks/useVerifyESGICertificates.tsx
@@ -1,30 +1,31 @@
 import { useReadContract } from "wagmi";
 import { ESGICertificatesAbi } from "@/utils/abis/ESGICertificates";
+import { ESGIAnnualPerformanceAbi } from "@/utils/abis/ESGIAnnualPerformance";
 
 const PROGRAM_NFT_ADDRESS = process.env.NEXT_PUBLIC_ESGIPROGRAMNFT_ADDRESS;
+const PERFORMANCE_NFT_ADDRESS =
+  process.env.NEXT_PUBLIC_ESGIPERFORMANCENFT_ADDRESS;
 
-/**
- * Hook to verify ESGI certificates
- * @param tokenId - Token ID of the certificate
- * @param address - Student wallet address (optional)
- */
-const useVerifyESGICertificates = (tokenId?: number, address?: string) => {
+export const useVerifyESGICertificates = (
+  type: "program" | "performance",
+  tokenId?: number
+) => {
   const {
     data: tokenExists,
     isError,
     isLoading,
   } = useReadContract({
-    abi: ESGICertificatesAbi,
-    address: PROGRAM_NFT_ADDRESS as `0x${string}`,
+    abi: type === "program" ? ESGICertificatesAbi : ESGIAnnualPerformanceAbi,
+    address: (type === "program"
+      ? PROGRAM_NFT_ADDRESS
+      : PERFORMANCE_NFT_ADDRESS) as `0x${string}`,
     functionName: "doesTokenExist",
     args: tokenId !== undefined ? [BigInt(tokenId)] : undefined,
   });
 
   return {
-    tokenExists: tokenExists as boolean | undefined, // Typage explicite
+    tokenExists: tokenExists as boolean,
     isLoading,
     isError,
   };
 };
-
-export { useVerifyESGICertificates };
