194a8d05469a8685de7943b87797dc766f77dede
0xNaxzerrr <naxdev75@gmail.com>
2025-02-10 07:08:14 +0100

    feat: Add TypeScript interfaces for minting and updating forms

---
+++ L3ARN-APP/src/utils/interfaces/interfaces.ts
@@ -0,0 +1,41 @@
+export interface MintForm {
+  studentAddress: string;
+  programName: string;
+  startYear: number;
+  endYear: number;
+  file: File | null;
+}
+
+export interface UpdateForm {
+  tokenId: string;
+  newStatus: string;
+}
+
+export interface UploadResult {
+  ipfsHash: string;
+  ipfsUrl: string;
+}
+
+export interface MintFormProps {
+  mintForm: MintForm;
+  handleFormChange: (
+    formName: "mint",
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => void;
+  handleSubmit: (formName: "mint", e: React.FormEvent<HTMLFormElement>) => void;
+  isUploading: boolean;
+  isReadyToMint: boolean;
+  isPending: boolean;
+}
+
+export interface UpdateFormProps {
+  updateForm: UpdateForm;
+  handleFormChange: (
+    formName: "update",
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => void;
+  handleSubmit: (
+    formName: "update",
+    e: React.FormEvent<HTMLFormElement>
+  ) => void;
+}
