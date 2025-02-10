e0a55638310aceb58f238a26012fd0a65f8edebf
0xNaxzerrr <naxdev75@gmail.com>
2025-02-10 07:07:57 +0100

    refactor: Clean up and reorganize imports and component structure in _app.tsx

--- L3ARN-APP/src/pages/_app.tsx
+++ L3ARN-APP/src/pages/_app.tsx
@@ -1,28 +1,28 @@
-import type { AppProps } from 'next/app';
-import Layout from '@/components/layout/layout';
+"use client";
+import type { AppProps } from "next/app";
+import Layout from "@/components/layout/layout";
 
-import '../styles/globals.css';
-import '@rainbow-me/rainbowkit/styles.css';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { WagmiProvider } from 'wagmi';
-import { RainbowKitProvider } from '@rainbow-me/rainbowkit';
-import { ESGI, config } from '../wagmi';
-import { Toaster } from "@/components/ui/toaster"
+import "../styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WagmiProvider } from "wagmi";
+import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { ESGI, config } from "../wagmi";
+import { Toaster } from "@/components/ui/toaster";
 
 const client = new QueryClient();
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-
     <WagmiProvider config={config}>
-    <QueryClientProvider client={client}>
-      <RainbowKitProvider initialChain={ESGI}>
-      <Layout>
-        <Component {...pageProps} />
-        <Toaster />
-        </Layout> 
-      </RainbowKitProvider>
-    </QueryClientProvider>
-  </WagmiProvider>
+      <QueryClientProvider client={client}>
+        <RainbowKitProvider initialChain={ESGI}>
+          <Layout>
+            <Component {...pageProps} />
+            <Toaster />
+          </Layout>
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   );
 }
 export default MyApp;
